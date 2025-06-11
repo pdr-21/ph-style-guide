821bbdfe5e10d3e63ad4b70ba11943abe4bf44bc
pdr-21 <pauldrotar97@gmail.com>
2025-06-11 09:10:13 +0300

    Fix Full-Width Borders with Proper Content Padding

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,12 +5,16 @@ import ProjectTable from '../components/projects/ProjectTable';
 const Projects: React.FC = () => {
   return (
     <div>
-      <div className="border-b border-n-75 pb-6 pt-6 px-8">
-        <ProjectFilterBar />
+      <div className="border-b border-n-75">
+        <div className="pb-6 pt-6 px-8">
+          <ProjectFilterBar />
+        </div>
       </div>
       
-      <div className="pt-8 px-8">
-        <ProjectTable />
+      <div className="border-b border-n-75">
+        <div className="pt-8 px-8 pb-8">
+          <ProjectTable />
+        </div>
       </div>
     </div>
   );
