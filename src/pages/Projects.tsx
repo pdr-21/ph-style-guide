c9f0893d945bd2d7da666158e79c60d1753005cb
pdr-21 <pauldrotar97@gmail.com>
2025-06-11 09:05:02 +0300

    Updated Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects: React.FC = () => {
   return (
     <div>
-      <div className="border-b border-n-75 pb-4 px-8">
+      <div className="border-b border-n-75 pb-4 pt-4 px-8">
         <h1 className="text-2xl font-semibold text-gray-800">Projects</h1>
         <p className="text-gray-600 mt-1">Manage your projects</p>
       </div>
