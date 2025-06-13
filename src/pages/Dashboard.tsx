e24f36dec26b2947f8d7a419da5d373f34d130f5
pdr-21 <pauldrotar97@gmail.com>
2025-06-13 14:05:41 +0300

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -19,7 +19,7 @@ const Dashboard: React.FC = () => {
         {/* Left column - 70% */}
         <div className="lg:col-span-7">
           {/* Gradient container */}
-          <div className="bg-gradient-to-bl from-b-40 to-white rounded-2xl p-8 mb-6 gap-y-20">
+          <div className="flex flex-col gap-6 bg-gradient-to-bl from-b-40 to-white rounded-2xl p-8 mb-6">
             {/* Summary header with icon */}
             <div className="flex items-center">
               <SparkleIcon className="mr-3" />
