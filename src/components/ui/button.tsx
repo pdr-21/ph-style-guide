26fd46dc37ba2b5d0b28b91e9269d1ffd8ce1e34
pdr-21 <pauldrotar97@gmail.com>
2025-06-13 10:33:21 +0300

    Updated button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "../../lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:shadow-focus-normal disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-medium transition-colors focus-visible:outline-none focus-visible:shadow-focus-normal disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
